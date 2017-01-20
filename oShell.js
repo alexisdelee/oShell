0e0a18f352456b4a52625439657c0f2703706561
Alexis Delée <alexis.delee@hotmail.fr>
2017-01-20 22:16:35 +0100

    shell

--- oShell.js
+++ oShell.js
@@ -1,7 +1,9 @@
 var inquirer = require("inquirer");
 var http = require("http");
+var fs = require("fs");
 var exec = require("child_process").exec;
 var os = require("os");
+var path = require("path");
 
 var networks = [];
 var ifaces = os.networkInterfaces();
@@ -32,18 +34,49 @@ inquirer.prompt([{
   else if(port < 1 || port > 47823) return console.log("error port");
 
   var server = http.createServer((req, res) => {
+    /* fs.readFile("./index.html", "utf-8", (error, content) => {
+      res.writeHead(200, {"Content-Type": "text/html"});
+      res.end(content);
+    }); */
+
+    var js = 'var socket=io.connect("' + network.address + ':' + port + '");var header=document.querySelector("header");var shell=document.querySelector("#search");var footer=document.querySelector("footer");document.body.addEventListener("keydown",(e)=>{if(e.keyCode==13){if(shell.value=="cls"||shell.value=="clear"){footer.innerHTML="";}else{socket.emit("message",shell.value);} shell.value="";}});socket.on("message",(message)=>{Object.keys(message).forEach((id)=>{if(!!message[id]){if(id=="basedir"){header.textContent=message[id];}else{footer.innerHTML="<div class=\\"" + id + "\\">"+message[id].replace(/\\n/g,"<br>")+"</div><br>"+footer.innerHTML;}}});});';
+    var css = '*{margin:0;padding:0;background:#2D2D2D}input:focus{outline-width:0}#shell{box-sizing:border-box;width:100%}header{display:inline-block;padding:4px 10px;background:#6998CC;font-family:"Lucida Console";font-size:14px;color:#2D2D2D}#search{display:inline-block;position:relative;left:0;padding:2px;border-color:transparent;font-family:"Lucida Console";color:#CCD7AF}footer{position:absolute;top:26px;left:4px;bottom:0px;right:4px;font-family:"Lucida Console";font-size:13px}.stdout{color:#D8C8B8}.stderr{color:#B63C2E;font-weight:bold}';
+
     res.writeHead(200, {"Content-Type": "text/html"});
-    res.write('<!DOCTYPE html><html> <head> <meta charset="utf-8"/> <title>oShell</title> <style type="text/css"> *{margin: 0; padding: 0;}#search{position: absolute; top: 10px; left: 0; padding-left: 10px; width: 100%; box-sizing: border-box; border-color: transparent;}</style> </head> <body> <input id="search" placeholder="Command line..." autofocus></input> <script src="/socket.io/socket.io.js"></script> <script>var socket=io.connect("' + network.address + ':' + port + '"); var shell=document.querySelector("#search"); document.body.addEventListener("keydown", (e)=>{if(e.keyCode==13){socket.emit("message", shell.value); shell.value="";}}); socket.on("message", (message)=>{Object.keys(message).forEach((id)=>{if(!!message[id]){console.log(message[id]);}});}); </script> </body></html>');
+    res.write('<!DOCTYPE html><html><head><meta charset="utf-8" /><title>oShell</title><style type="text/css">' + css + '</style></head><body><div id="shell"> <header></header> <input id="search" autofocus></input> <footer></footer></div> <script src="/socket.io/socket.io.js"></script> <script>' + js + '</script> </body></html>');
     res.end();
   });
 
   var io = require("socket.io").listen(server);
+  var basedir = path.dirname("oShell.js"), restorePath = "";
 
   io.sockets.on("connection", (socket) => {
+    socket.emit("message", {basedir: basedir});
+
     socket.on("message", (message) => {
-      exec(message, (error, stdout, stderr) => {
-        socket.emit("message", {stdout: stdout, stderr: stderr});
-      });
+      command = message.split(" ");
+      if(command[0] == "cd" && command.length == 2){
+        restorePath = basedir;
+
+        if(path.isAbsolute(command[1])){
+          basedir = command[1];
+        } else {
+          basedir = path.resolve(basedir + path.sep, command[1]);
+        }
+
+        fs.stat(basedir, (err, stats) => {
+          if(err || !stats.isDirectory()){
+            basedir = restorePath;
+            return;
+          } else {
+            socket.emit("message", {basedir: basedir});
+          }
+        });
+      } else {
+        exec(message, {cwd: basedir}, (error, stdout, stderr) => {
+          socket.emit("message", {stdout: stdout, stderr: stderr});
+        });
+      }
     });
   });
 
